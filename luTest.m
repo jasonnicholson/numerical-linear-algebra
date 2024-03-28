64c6e66615e86eff68a2bb0313d0c6b51d0503de
Jason H. Nicholson <1058191+jasonnicholson@users.noreply.github.com>
2024-03-28 21:48:19 +0000

    Updated whitespace. Removed arguments block from all LU algorithms.

--- luTest.m
+++ luTest.m
@@ -7,10 +7,11 @@ clc; clear; close all;
 
 %% Simple single matrix test
 rng default;
-n = 1875;
+n = 500;
 A = rand(n);
 % A = rand(n);
 b = rand(n,1);
+tol = 1e-13;
 
 Algorithms = ["\"; "luWithPartialPivoting"; "luWithRookPivoting"; "luWithCompletePivoting"; "gaussianEliminationWithPartialPivoting"];
 
@@ -19,8 +20,8 @@ results.Time(1) = timeit(@() A\b,1);
 x1 = A\b;
 results.NormDiff(1) = 0;
 for i=numel(Algorithms):-1:2
-    results.Time(i) = timeit(@() feval(Algorithms(i),A,b),1);
-    x = feval(Algorithms(i),A,b);
+    results.Time(i) = timeit(@() feval(Algorithms(i),A,b,tol),1);
+    x = feval(Algorithms(i),A,b,tol);
     results.NormDiff(i) = norm(x1 - x);
 end
 

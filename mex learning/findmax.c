93099bc859ad050680dc0a1b971ca866c1843d0c
Jason H. Nicholson <1058191+jasonnicholson@users.noreply.github.com>
2024-03-30 21:02:31 -0400

    compiled findmax.c with -Wall. Found issues with the signedness of m and increment variables. idamax_ expected signed.

--- mex learning/findmax.c
+++ mex learning/findmax.c
@@ -5,8 +5,8 @@ void mexFunction(int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[]) {
   // Setup inputs
   const mxDouble *x = mxGetDoubles(prhs[0]);
   const mwIndex column = (mwIndex) mxGetScalar(prhs[1]) - 1;
-  const mwSize m = mxGetM(prhs[0]);
-  const mwIndex increment = 1;
+  const mwSignedIndex m = mxGetM(prhs[0]);
+  const mwSignedIndex increment = 1;
 
   // Setup outputs
   plhs[0] = mxCreateDoubleMatrix(1, 1, mxREAL);

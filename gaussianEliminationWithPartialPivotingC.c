ce82f312144729ab37b1bbdd830ab7f2b29cdab4
Jason H. Nicholson <1058191+jasonnicholson@users.noreply.github.com>
2024-03-31 20:25:56 -0400

    renamed variables with more descriptive names.
    Fixed a bug with the rank check. The diagonal was not searched. The index was used incorrectly.

--- gaussianEliminationWithPartialPivotingC.c
+++ gaussianEliminationWithPartialPivotingC.c
@@ -8,9 +8,10 @@
 void mexFunction(int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[]) {
   // Get the size of the matrix A and b vector
   // Normally we would use mwSize, but we are using mwSignedIndex to avoid warnings with BLAS functions when using -Wall.
-    mwSignedIndex n = mxGetM(prhs[0]);
-    mwSignedIndex nb = mxGetN(prhs[1]);
-    mwSignedIndex nnb = n + nb;
+    mwSignedIndex nRowsA = mxGetM(prhs[0]);
+    mwSignedIndex nColumnsA = nRowsA;
+    mwSignedIndex nColumnsb = mxGetN(prhs[1]);
+    mwSignedIndex nColumnsAandb = nRowsA + nColumnsb;
 
     // Check for proper number of input and output arguments
     if (nrhs != 3) {
@@ -25,17 +26,14 @@ void mexFunction(int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[]) {
     if (!mxIsDouble(prhs[0]) || 
         !mxIsDouble(prhs[1]) || 
         !mxIsDouble(prhs[2])) {
-        mexErrMsgIdAndTxt("MATLAB:gaussianEliminationWithPartialPivoting:inputNotRealScalarDouble",
-                          "Input arguments must be real double.");
+        mexErrMsgIdAndTxt("MATLAB:gaussianEliminationWithPartialPivoting:inputNotRealScalarDouble", "Input arguments must be real double.");
     }
     // Check that A and b are compatible
-    if (mxGetN(prhs[0]) != n) {
-        mexErrMsgIdAndTxt("MATLAB:gaussianEliminationWithPartialPivoting:invalidInput",
-                          "Matrix A must be square.");
+    if (mxGetN(prhs[0]) != nRowsA) {
+        mexErrMsgIdAndTxt("MATLAB:gaussianEliminationWithPartialPivoting:invalidInput", "Matrix A must be square.");
     }
-    if (mxGetM(prhs[1]) != n) {
-        mexErrMsgIdAndTxt("MATLAB:gaussianEliminationWithPartialPivoting:invalidInput",
-                          "Matrix A and vector b must have the same number of rows.");
+    if (mxGetM(prhs[1]) != nRowsA) {
+        mexErrMsgIdAndTxt("MATLAB:gaussianEliminationWithPartialPivoting:invalidInput",  "Matrix A and vector b must have the same number of rows.");
     }
 
     // Get the inputs
@@ -43,57 +41,61 @@ void mexFunction(int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[]) {
     mxDouble *b = mxGetDoubles(prhs[1]);
     mxDouble tol = mxGetScalar(prhs[2]);
 
-    // Combining A and b signifies most of the data needed in cache for processing
-    mxArray *Ab_mxArray = mxCreateDoubleMatrix(n, nnb, mxREAL);
+    // Combining A and b. This the main data needed in cache for the algorithm.
+    mxArray *Ab_mxArray = mxCreateDoubleMatrix(nRowsA, nColumnsAandb, mxREAL);
     mxDouble *Ab = mxGetDoubles(Ab_mxArray);
-    memcpy(Ab, A, n*n*sizeof(mxDouble));
-    memcpy(Ab + n*n, b, n*nb*sizeof(mxDouble));
+    memcpy(Ab, A, nRowsA*nRowsA*sizeof(mxDouble));
+    memcpy(Ab + nRowsA*nRowsA, b, nRowsA*nColumnsb*sizeof(mxDouble));
     
     // Perform gaussian elimination with partial pivoting
-    for (mwIndex k = 0; k < n-1; k++) {
+    for (mwIndex kColumn = 0; kColumn < nColumnsA-1; kColumn++) {
+        mwIndex kRow = kColumn;
         //Find the pivot index by looking down the column
-        ptrdiff_t increment = 1; // defines how big of step to take through the vector.
+        mwSignedIndex increment = 1; // defines how big of step to take through the vector.
         // Call BLAS function idamax_ . idamax_ returns 1-based indexing.
-        ptrdiff_t nn = (ptrdiff_t)(n-k);
-        mwIndex j = idamax_(&nn, Ab + k*n + k, &increment) - 1 + k; // Subtract 1 to get 0-based indexing
+        mwSignedIndex nRowsInKcolumn = nRowsA - (mwSignedIndex)kRow;
+        mwSignedIndex jRow = idamax_(&nRowsInKcolumn, Ab + kColumn*nRowsA + kRow, &increment) - 1 + kRow; // Subtract 1 to get 0-based indexing
 
         // Swap the k-th and j-th rows in Ab
         // Use dswap_ routine from BLAS
-        dswap_(&nnb, Ab + j, &n, Ab + k, &n);
+        dswap_(&nColumnsAandb, Ab + jRow, &nRowsA, Ab + kColumn, &nRowsA);
 
         // Perform the gaussian elimination
-        if (Ab[k + k*n] == 0) {
+        if (Ab[kRow + kColumn*nRowsA] == 0) {
             mexErrMsgIdAndTxt("MATLAB:gaussianEliminationWithPartialPivoting:matrixIsSingular", "Matrix is singular.");
         } else {
-            for (mwIndex i = k+1; i < n; i++) {
-                double lVector = Ab[i + k*n] / Ab[k + k*n]; // Calculate lower triangular part
-                for (mwIndex j = k+1; j < nnb; j++) {
-                    Ab[i + j*n] -= lVector * Ab[k + j*n]; // Calculate upper triangular part
+            for (mwIndex iRow = kRow+1; iRow < nRowsA; iRow++) {
+                Ab[iRow + kColumn*nRowsA] = Ab[iRow + kColumn*nRowsA] / Ab[kRow + kColumn*nRowsA]; // Calculate lower triangular part
+                for (mwIndex jColumn = kColumn+1; jColumn < nColumnsAandb; jColumn++) {
+                    Ab[iRow + jColumn*nRowsA] -= Ab[iRow + kColumn*nRowsA] * Ab[kRow + jColumn*nRowsA]; // Calculate upper triangular part
                 }
             }
         }
     }
 
     // Check for rank deficiency or near singularity
-    mwSignedIndex maxDiagIndex = idamax_(&n, Ab, &n) - 1; // find the maximum diagonal element
-    mxDouble maxDiag = fabs(Ab[maxDiagIndex]);
-    for (mwIndex i = 0; i < n; i++) {
-        if (fabs(Ab[i + i*n])/maxDiag < tol) {
+    mwSignedIndex incrementBetweenDiagonalElements = nRowsA + 1; // defines how big of step to take through the vector.
+    mwSignedIndex maxDiagIndex = idamax_(&nRowsA, Ab, &incrementBetweenDiagonalElements) - 1; // find the maximum diagonal element
+    mxDouble maxDiag = fabs(Ab[maxDiagIndex + maxDiagIndex*nRowsA]);
+    for (mwIndex iColumn = 0; iColumn < nColumnsA; iColumn++) {
+        mwIndex iRow = iColumn;
+        if (fabs(Ab[iRow + iColumn*nRowsA])/maxDiag < tol) {
             mexWarnMsgIdAndTxt("MATLAB:gaussianEliminationWithPartialPivoting:matrixIsCloseToSingular", "Matrix is close to singular or badly scaled. Results may be inaccurate.");
             break;
         }
     }
 
     // Backward substitution to solve Ux = y
-    plhs[0] = mxCreateDoubleMatrix(n, nb, mxREAL);
+    plhs[0] = mxCreateDoubleMatrix(nRowsA, nColumnsb, mxREAL);
     mxDouble *x = mxGetDoubles(plhs[0]);
-    for (mwIndex i = n; i-- > 0;) {
-        for (mwIndex j = 0; j < nb; j++) {
-            x[i + j*n] = Ab[i + (j+n)*n] / Ab[i + i*n];
+    for (mwIndex iRow = nRowsA; iRow-- > 0;) {
+        mwIndex iColumn = iRow;
+        for (mwIndex jColumnb = 0; jColumnb < nColumnsb; jColumnb++) {
+            x[iRow + jColumnb*nRowsA] = Ab[iRow + (jColumnb+nColumnsA)*nRowsA] / Ab[iRow + iColumn*nRowsA];
         }
-        for (mwIndex j = 0; j < i; j++) {
-            for (mwIndex k = 0; k < nb; k++) {
-                Ab[j + (k+n)*n] -= Ab[j + i*n] * x[i + k*n];
+        for (mwIndex jRow = 0; jRow < iRow; jRow++) {
+            for (mwIndex kColumnb = 0; kColumnb < nColumnsb; kColumnb++) {
+                Ab[jRow + (kColumnb+nColumnsA)*nRowsA] -= Ab[jRow + iColumn*nRowsA] * x[iRow + kColumnb*nRowsA];
             }
         }
     }
